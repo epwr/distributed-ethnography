7ab8dcc959d37cf06858bc48528d5a13f8334f5a
Eric Power <ericmarkpower@gmail.com>
2024-02-01 07:51:10 -0800

    test database now has initial data

---
+++ tests/test_assets/test_db_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO survey (
+	   uid
+	   , is_open
+	   , name
+) VALUES (
+  "00000000-9c88-4b81-9de4-bac7444fbb0a"
+  , false
+  , "Closed Test Survey"
+),
+(
+  "00000000-a087-4fb6-a123-24ff30263530"
+  , true
+  , "Open Test Survey"
+);
